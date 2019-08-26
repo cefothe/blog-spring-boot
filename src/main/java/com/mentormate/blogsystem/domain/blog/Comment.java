cae4e7df7a5f4f9a3b6bb88d35e7fac8610daf92
cefothe <cefothe@gmail.com>
2019-08-26 23:27:38 +0300

    disable spring security add swagger configuration and fix problem with comment entity

--- src/main/java/com/mentormate/blogsystem/domain/blog/Comment.java
+++ src/main/java/com/mentormate/blogsystem/domain/blog/Comment.java
@@ -1,12 +1,13 @@
 package com.mentormate.blogsystem.domain.blog;
 
+import com.mentormate.blogsystem.domain.BaseEntity;
 import lombok.Data;
 
 import javax.persistence.Entity;
 
 @Data
 @Entity
-public class Comment {
+public class Comment extends BaseEntity {
     private String title;
     private String content;
 }
